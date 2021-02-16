ce0d71f0fda10e743564da2edb090a4f27417f27
Kristen Fang <fang.kristen@gmail.com>
2021-02-15 19:47:00 -0500

    netlify v2

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -3,17 +3,21 @@ import React from 'react'
 export default function ContactForm() {
     return (
     <>
-        <form name="contact" netlify>
-        <p>
-            <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
             <button type="submit">Send</button>
-        </p>
-        </form>
+          </p>
+        </form>,
     </>
     )
 }
