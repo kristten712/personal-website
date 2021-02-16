d6937b7c424f3d8b145f8de685b75660762bfdf6
Kristen Fang <fang.kristen@gmail.com>
2021-02-15 19:53:52 -0500

    netlify v3

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Container from '@material-ui/core/Container'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
 
 export default function ContactForm() {
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+      if ( window.location.search.includes('success=true') ) {
+        setSuccess(true);
+      }
+    }, []);
+
     return (
     <>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>,
+<Container maxWidth='sm'>
+ <h2>CONTACT</h2>
+   <Card>
+     <form 
+       name="contact" 
+       method="POST" 
+       data-netlify="true" 
+      >
+     <input type="hidden" name="form-name" value="contact" />
+        <TextField id="standard-basic" label="name" name="name" />
+        <TextField id="standard-basic" label="email" name="email" />
+        <TextField multiline id="standard-basic" label="message" name="message" />
+        <Button type="submit">Send</Button>
+       </form>
+      </Card>
+   </Container>
     </>
     )
 }
